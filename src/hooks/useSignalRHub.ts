79c47de220008bd80ee8c61464d6cdd153d4aded
jagerbob <jagerbob@users.noreply.github.com>
2022-07-04 22:47:53 +0200

    Adding ssl support + small fixes

--- src/hooks/useSignalRHub.ts
+++ src/hooks/useSignalRHub.ts
@@ -1,13 +1,15 @@
-import { HubConnection, HubConnectionBuilder, LogLevel } from "@microsoft/signalr"
+import { HubConnection, HubConnectionBuilder, JsonHubProtocol, LogLevel } from "@microsoft/signalr"
 import { useEffect, useState } from "react"
 
 export const useSignalRHub = () => {
-  const [ hub, setHub ] = useState<HubConnection>();
+  const [hub, setHub] = useState<HubConnection>();
 
   useEffect(() => {
     const getHub = async () => {
+      console.log("URL: " + process.env.REACT_APP_WS_SERVER_URL);
+      console.log("PORT: " + process.env.REACT_APP_WS_SERVER_PORT);
       const connection = new HubConnectionBuilder()
-        .withUrl("https://localhost:7255/pokerHub")
+        .withUrl(`https://${process.env.REACT_APP_WS_SERVER_URL}:${process.env.REACT_APP_WS_SERVER_PORT}/pokerHub`)
         .configureLogging(LogLevel.Information)
         .build();
       await connection.start();
